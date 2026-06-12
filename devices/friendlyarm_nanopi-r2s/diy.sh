324255084036f05905f2e887d4de4cb3c5ed951c
las <218048983+wingwongs@users.noreply.github.com>
2026-06-12 22:50:30 +0800

    Update diy.sh

--- devices/friendlyarm_nanopi-r2s/diy.sh
+++ devices/friendlyarm_nanopi-r2s/diy.sh
@@ -4,6 +4,9 @@
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 /bin/bash "$SHELL_FOLDER/../rockchip_armv8/diy.sh"
 
+sed -i "s/CONFIG_PACKAGE_kmod-shortcut-fe=y/# CONFIG_PACKAGE_kmod-shortcut-fe is not set/" .config
+sed -i "s/CONFIG_PACKAGE_kmod-shortcut-fe-cm=y/# CONFIG_PACKAGE_kmod-shortcut-fe-cm is not set/" .config
+sed -i "s/CONFIG_PACKAGE_kmod-shortcut-fe-dns=y/# CONFIG_PACKAGE_kmod-shortcut-fe-dns is not set/" .config
 
 # 创建首开机脚本以设定网络配置、网口和IP/DNS等
 mkdir -p files/etc/uci-defaults
