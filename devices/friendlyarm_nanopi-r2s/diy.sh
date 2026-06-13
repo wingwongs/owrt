5c551878cda8f40353c9a4b7b3a46a58ac47fb7c
las <218048983+wingwongs@users.noreply.github.com>
2026-06-13 15:56:32 +0800

    Fix package removal paths in diy.sh
    Updated paths for removing packages and fixed comments.

--- devices/friendlyarm_nanopi-r2s/diy.sh
+++ devices/friendlyarm_nanopi-r2s/diy.sh
@@ -4,13 +4,14 @@
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 /bin/bash "$SHELL_FOLDER/../rockchip_armv8/diy.sh"
 
+shopt -s extglob
 # 解决 linux-6.12.92 下 953-net-patch-linux-kernel-to-support-shortcut-fe.patch 冲突问题
 # 在编译 rockchip_armv8 架构时，直接删除此补丁，因为 SFE 在 6.12 内核中通常需要清理此残留补丁以防冲突
 rm -f target/linux/generic/hack-6.12/952-add-net-conntrack-events-support-multiple-registrant.patch
 rm -f target/linux/generic/hack-6.12/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
 rm -f target/linux/generic/hack-6.12/930-Revert-Revert-Revert-driver-core-Set-fw_devlink-on-b.patch
-rm -rf package/feeds/kiddin9/luci-app-turboacc
-rm -rf package/feeds/kiddin9/shortcut-fe
+rm -rf feeds/kiddin9/luci-app-turboacc
+rm -rf feeds/kiddin9/shortcut-fe
 #sed -i "s/CONFIG_PACKAGE_kmod-shortcut-fe=y/# CONFIG_PACKAGE_kmod-shortcut-fe is not set/" .config
 
 # 创建首开机脚本以设定网络配置、网口和IP/DNS等
